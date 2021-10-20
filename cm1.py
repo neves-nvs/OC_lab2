705bd32cd4429c921d791c7fcb3ae059439ea698
Diogo Ferreira <57763474+neves-nvs@users.noreply.github.com>
2021-10-20 21:31:23 +0100

    added plots for cm1

---
+++ cm1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+
+with open("cm1_l2.out") as f:
+    data = f.read().split("\n")[:-1]
+
+
+class Data():
+    def __init__(self, line):
+        line = line.split()
+        self.size = int(line[0].split("=")[1])
+        self.stride = int(line[1].split("=")[1])
+        self.misses = float(line[2].split("=")[1])
+        self.time = float(line[3].split("=")[1])
+
+    def __repr__(self):
+        return "{}\t{}\t{}".format(self.stride, self.misses, self.time)
+
+
+res = {}
+
+for line in data:
+    key = int(line.split()[0].split("=")[1])
+    if key in res.keys():
+        res[key].append(Data(line))
+    else:
+        res[key] = [Data(line)]
+
+for k in res:
+    print("Array Size: {}".format(k))
+    print("Stride\tMisses\tTime")
+    for data in res[k]:
+        print(data)
+
+
+for k in res:
+    strides = list(map(lambda x: x.stride, res[k]))
+    misses = list(map(lambda x: x.misses, res[k]))
+    plt.plot(strides, misses, label=str(k >> 10)+"k")
+    plt.legend()
+    plt.xscale("log", base=2)
+
+plt.show()
